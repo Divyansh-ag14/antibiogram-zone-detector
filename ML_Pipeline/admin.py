77632e0da299c139ef95207191fd004234935f8e
Divyansh Agarwal <divyanshagarwal188@gmail.com>
2025-06-05 01:28:36 -0400

    path management

---
+++ ML_Pipeline/admin.py
@@ -0,0 +1,7 @@
+import os
+
+train_dir = os.path.join("", "Input", "Data")
+output_path = os.path.join("", "Output")
+
+# print(f"Train Directory: {train_dir}")
+# print(f"Output Path: {output_path}")
